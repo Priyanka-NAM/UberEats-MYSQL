8601975bda1be2c4661ab54d9146f720bc53362f
Priyanka <alivelumanjulokapriyanka.nareddy@sjsu.edu>
2021-09-04 21:25:02 -0700

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import Main from "./components/main";
+function App() {
+  return (
+    <fragment>
+      <main className='container'>
+        {/* <Switch>
+          <Main />
+        </Switch> */}
+      </main>
+    </fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <fragment>
+      <main className='container'>
+        <switch>
+          {/* <Route path='/signin' component={SignIn}></Route>
+        <Route path='/signup' component={SignUp}></Route>
+        <Route path='/' exact component={HomePage}></Route> */}
+        </switch>
+      </main>
+    </fragment>
+  );
+}
