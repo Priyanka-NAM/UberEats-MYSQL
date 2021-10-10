f28e82b43c0daa23cedf67a17ac03be9dc7d7195
Priyanka <alivelumanjulokapriyanka.nareddy@sjsu.edu>
2021-10-10 00:21:25 -0700

    Added AWS Db connection

--- backend/dbPoolConnection.js
+++ backend/dbPoolConnection.js
@@ -1,32 +1,32 @@
 const configure = require("config");
 
-const mysql = require("mysql");
+const mysql = require("mysql2");
 // const mysql = require("promise-mysql");
+
+const constants = require("./config.json");
 
 require("dotenv").config();
 
-// if (!configure.get("jwtPrivateKey")) {
-//   console.log("FATAL ERROR: jwtPrivateKet is not defined");
-//   process.exit(1);
-// }
-// Pool supports pool of requests.
-
-// for aws
-// DB_HOST=ubereats.cgpwh4cefk7y.us-east-2.rds.amazonaws.com
-// DB_USER=admin
-// DB_PASS=ubereats
-// DB_CONNECTIONLIMIT=500
-// DB_PORT=3306
+// const dbPoolConnection = mysql.createPool({
+//   connectionLimit: 200,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   port: process.env.DB_PORT,
+//   password: process.env.DB_PASS,
+//   database: "uber_eats_test",
+//   insecureAuth: true,
+// });
 
 const dbPoolConnection = mysql.createPool({
-  connectionLimit: 200,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password: process.env.DB_PASS,
-  database: "uber_eats_test",
-  insecureAuth: true,
+  connectionLimit: 500,
+  host: constants.DB.host,
+  user: constants.DB.user,
+  port: constants.DB.port,
+  password: constants.DB.password,
+  database: constants.DB.database,
+  // insecureAuth: true,
 });
+
 dbPoolConnection.getConnection((error) => {
   if (error) {
     throw error;
